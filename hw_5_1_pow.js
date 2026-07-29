787b43ac04d54101fadf17df5e978ea2ca0c1e50
zemercvil <zemercvil@gmail.com>
2026-07-29 20:06:50 +0300

    pow function

--- hw_5_1_pow.js
+++ hw_5_1_pow.js
@@ -1,3 +1,29 @@
-function pow(2, 3){
+let x = 7;
+let y = 2;
+
+
+function pow(x,y) {
+    let exponentiation = x;
+    if (y === 0)
+    {
+        exponentiation = 1;
+    }
+    else if (y < 0)
+    {
+        console.log(`Значення ступеню негативне = ${y}. Введіть значення y >=0.`)
+        return null;
+    }
+    else 
+    {
+            for (let i = 2; i <= y; i++)
+            {
     
+                exponentiation = exponentiation * x;
+
+            }
+    }
+    return exponentiation;
+
 }
+const result = pow(x, y);
+console.log(`Результат возведення ${x} в ступінь ${y} = ${result}`)
